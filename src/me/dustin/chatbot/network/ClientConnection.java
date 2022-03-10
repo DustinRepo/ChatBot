e5584db9c6602fb6e62fc0140957950a3a4a4cfa
DustinRepo <tinmann4300@gmail.com>
2022-03-10 14:27:01 -0500

    Fixed typo

--- src/me/dustin/chatbot/network/ClientConnection.java
+++ src/me/dustin/chatbot/network/ClientConnection.java
@@ -44,7 +44,7 @@ public class ClientConnection {
 
     private long lastAnnouncement = -1;
     private long lastKeepAlive = -1;
-    private final String[] announcements = new String[]{"Use {PREFIX}help to get a list of my commands", "Use {PREFIX}worstping to see who has the highest ping", "Use {PREFIX}bestping to see who has the highest ping", "Use {PREFIX}coffee to get a picture of coffee", "Need to report someone? Use {PREFIX}report <name> <reason>", "Use {PREFIX}isEven to see if a number is even!", "Need to see server TPS? {PREFIX}tps", "Want to use this bot program? https://github.com/DustinRepo/ChatBot"};
+    private final String[] announcements = new String[]{"Use {PREFIX}help to get a list of my commands", "Use {PREFIX}worstping to see who has the highest ping", "Use {PREFIX}bestping to see who has the lowest ping", "Use {PREFIX}coffee to get a picture of coffee", "Need to report someone? Use {PREFIX}report <name> <reason>", "Use {PREFIX}isEven to see if a number is even!", "Need to see server TPS? {PREFIX}tps", "Want to use this bot program? https://github.com/DustinRepo/ChatBot"};
 
     public ClientConnection(String ip, int port, Session session) throws IOException {
         this.ip = ip;
