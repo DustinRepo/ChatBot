37ff15190978f6defd799c8a0f202a49ce5f00b0
DustinRepo <tinmann4300@gmail.com>
2022-03-10 17:25:02 -0500

    Added new output to cum command

--- src/me/dustin/chatbot/command/impl/CommandCum.java
+++ src/me/dustin/chatbot/command/impl/CommandCum.java
@@ -1,6 +1,7 @@
 package me.dustin.chatbot.command.impl;
 
 import me.dustin.chatbot.command.Command;
+import me.dustin.chatbot.network.player.OtherPlayer;
 
 import java.util.Random;
 import java.util.UUID;
@@ -11,13 +12,16 @@ public class CommandCum extends Command {
         super("cum");
     }
 
-    String[] cumMessages = new String[]{"Oh fuck im gonna cum! AAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHH", "*nuts in your shoes* uwu did I do that?", "oh fuck don't stop I'm so close", "I'm about to bust a load so fat I'm gonna drown in it", "oh fuck, now I'm all sticky"};
+    String[] cumMessages = new String[]{"oh fuck {SENDER} you made me cum", "Oh fuck im gonna cum! AAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHH", "*nuts in your shoes* uwu did I do that?", "oh fuck don't stop I'm so close", "I'm about to bust a load so fat I'm gonna drown in it", "oh fuck, now I'm all sticky"};
 
     @Override
     public void run(String str, UUID send) {
+        OtherPlayer player = getClientConnection().getPlayerManager().get(send);
         int size = cumMessages.length;
         Random random = new Random();
         int select = random.nextInt(size);
-        sendChat(cumMessages[select]);
+        String message = cumMessages[select];
+        String name = player == null ? "you" : player.getName();
+        sendChat(message.replace("{SENDER}", name));
     }
 }
