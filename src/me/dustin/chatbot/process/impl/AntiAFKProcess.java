3b3d39c3db713befd7dd1493e48b1d6f79ab1a0c
DustinRepo <tinmann4300@gmail.com>
2022-03-13 10:23:17 -0400

    Code cleaning

--- src/me/dustin/chatbot/process/impl/AntiAFKProcess.java
+++ src/me/dustin/chatbot/process/impl/AntiAFKProcess.java
@@ -1,6 +1,7 @@
 package me.dustin.chatbot.process.impl;
 
 import me.dustin.chatbot.ChatBot;
+import me.dustin.chatbot.helper.Timer;
 import me.dustin.chatbot.network.ClientConnection;
 import me.dustin.chatbot.network.packet.c2s.play.ServerBoundPlayerRotationPacket;
 import me.dustin.chatbot.network.packet.c2s.play.ServerBoundPlayerSwingPacket;
@@ -9,26 +10,25 @@ import me.dustin.chatbot.process.ChatBotProcess;
 import java.util.Random;
 
 public class AntiAFKProcess extends ChatBotProcess {
-    private long lastAttempt = System.currentTimeMillis();
-
+    private final Timer timer = new Timer();
     public AntiAFKProcess(ClientConnection clientConnection) {
         super(clientConnection);
     }
 
     @Override
     public void init() {
-        lastAttempt = System.currentTimeMillis();
+        timer.reset();
     }
 
     @Override
     public void tick() {
-        if (System.currentTimeMillis() - lastAttempt >= ChatBot.getConfig().getAntiAFKDelay() * 1000L) {
+        if (timer.hasPassed(ChatBot.getConfig().getAntiAFKDelay() * 1000L)) {
             Random random = new Random();
             float yaw = random.nextInt(360) - 180;
             float pitch = random.nextInt(180) - 90;
             getClientConnection().sendPacket(new ServerBoundPlayerRotationPacket(yaw, pitch, true));
             getClientConnection().sendPacket(new ServerBoundPlayerSwingPacket(ServerBoundPlayerSwingPacket.MAIN_HAND));
-            lastAttempt = System.currentTimeMillis();
+            timer.reset();
         }
     }
 
