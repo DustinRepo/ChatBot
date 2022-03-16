1de667adfa63407c75a3ebc8adc516ce83f9e6a4
DustinRepo <tinmann4300@gmail.com>
2022-03-16 19:58:37 -0400

    Moved announcement list to announcements.json

--- src/me/dustin/chatbot/process/impl/AnnouncementProcess.java
+++ src/me/dustin/chatbot/process/impl/AnnouncementProcess.java
@@ -1,11 +1,17 @@
 package me.dustin.chatbot.process.impl;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import me.dustin.chatbot.ChatBot;
+import me.dustin.chatbot.chat.ChatMessage;
+import me.dustin.chatbot.command.CommandManager;
+import me.dustin.chatbot.helper.GeneralHelper;
 import me.dustin.chatbot.helper.StopWatch;
 import me.dustin.chatbot.network.ClientConnection;
 import me.dustin.chatbot.network.packet.c2s.play.ServerBoundChatPacket;
 import me.dustin.chatbot.process.ChatBotProcess;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -14,20 +20,15 @@ public class AnnouncementProcess extends ChatBotProcess {
     private final ArrayList<String> announcements = new ArrayList<>();
     public AnnouncementProcess(ClientConnection clientConnection) {
         super(clientConnection);
-        announcements.add("Use {PREFIX}help to get a list of my commands");
-        announcements.add("I can try to grab the server's plugins! use !plugins");
-        announcements.add("I can get you a player's skin directly from Minecraft! Use !skin <name>");
-        announcements.add("Use {PREFIX}coinflip to flip a coin");
-        announcements.add("Use {PREFIX}worstping or {PREFIX}bestping to see who has the lowest/highest ping");
-        announcements.add("Use {PREFIX}coffee to get a picture of coffee");
-        announcements.add("Need to report someone? Use {PREFIX}report <name> <reason>");
-        announcements.add("Use {PREFIX}isEven to see if a number is even!");
-        announcements.add("Need to see server TPS? {PREFIX}tps");
-        announcements.add("Want to use this bot program? It supports all versions from 1.12 - 1.18.2! https://github.com/DustinRepo/ChatBot");
-        if (ChatBot.getConfig().is2b2tCount())
-            announcements.add("Use {PREFIX}2b2tcount <name> to see how many times someone has mentioned 2b2t!");
-        if (ChatBot.getConfig().isQuotes())
-            announcements.add("Use {PREFIX}quote <name> to get a random quote from a player!");
+        File customCommandsFile = new File(new File("").getAbsolutePath(), "announcements.json");
+        if (customCommandsFile.exists()) {
+            JsonArray array = GeneralHelper.gson.fromJson(GeneralHelper.readFile(customCommandsFile), JsonArray.class);
+            for (int i = 0; i < array.size(); i++) {
+                announcements.add(array.get(i).getAsString());
+            }
+        } else {
+            GeneralHelper.print("No announcement file found!", ChatMessage.TextColors.DARK_RED);
+        }
     }
 
     @Override
