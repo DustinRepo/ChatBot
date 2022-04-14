c5983e113ae3718d188e08a82e05829476e840a6
DustinRepo <tinmann4300@gmail.com>
2022-04-13 20:24:06 -0400

    Added BlockPos class

---
+++ java/src/me/dustin/chatbot/network/world/BlockPos.java
@@ -0,0 +1,56 @@
+package me.dustin.chatbot.network.world;
+
+import java.util.Objects;
+
+public class BlockPos {
+    private int x, y, z;
+
+    public BlockPos(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public BlockPos(double x, double y, double z) {
+        this.x = (int)x;
+        this.y = (int)y;
+        this.z = (int)z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlockPos blockPos = (BlockPos) o;
+        return x == blockPos.x && y == blockPos.y && z == blockPos.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y, z);
+    }
+}
